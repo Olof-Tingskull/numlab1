41b89bc8a4ca16fa470d582d246d12c51e1fbfe8
Olof-Tingskull <olof.tingskull@gmail.com>
2022-12-05 17:55:54 +0100

    name change

--- src/uppgift7.py
+++ src/uppgift7.py
@@ -10,12 +10,12 @@ def f(x):
     return min(max(x / d, 0), 1) * v_max
 
 def fixed_backward_euler(current_pos, front_pos, front_new_pos, iterations=100, tolerans=10^-10):
-    calculate_difference = lambda x: abs(x - (current_pos + time_step * f(front_new_pos - x)))
+    calculate_error = lambda x: abs(x - (current_pos + time_step * f(front_new_pos - x)))
 
     x = current_pos
 
     for _ in range(iterations):
-        if calculate_difference(x) < tolerans: break
+        if calculate_error(x) < tolerans: break
 
         x = current_pos + time_step * f(front_new_pos - x)
 
